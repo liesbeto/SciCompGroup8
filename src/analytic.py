19c89bd57966908f636af942695e2e67666b23ab
fredmloth <frederiekeloth@gmail.com>
2025-02-20 09:38:41 +0100

    working analytical solution

--- src/analytic.py
+++ src/analytic.py
@@ -7,16 +7,18 @@ from scipy.special import erfc
 
 
 #introduce tolerance conditions later
-def analytic_sol(x, D, t, max_range):
+def analytic_sol(x, t, D, max_range):
     """
     D: diffusion coefficient
     x: space/place
     t: time at which to evaluate
     max_range: """
 
-    sum_analytical = 0
+    sum_analytical = np.zeros_like(x)
     for i in range(max_range):
-        sum_analytical += erfc((1 - x + 2*i) / (2*np.sqrt(D*t))) - erfc((1 + x + 2 * i) / (2*np.sqrt(D*t)))
+        term1 = erfc((1 - x + 2 * i) / (2*np.sqrt(D*t)))
+        term2 = erfc((1 + x + 2 * i) / (2*np.sqrt(D*t)))
+        sum_analytical += term1 - term2
 
     return sum_analytical
 
@@ -64,42 +66,47 @@ dt = dx**2 / (4 * D)
 
 
 
-grid = get_init_2Dgrid(N)
+    # grid = get_init_2Dgrid(N)
 
 
-fig, ax = plt.subplots()
-heatmap = ax.imshow(grid, cmap="hot", interpolation="nearest", vmin=0, vmax=1)
-plt.colorbar(heatmap)
+    # fig, ax = plt.subplots()
+    # heatmap = ax.imshow(grid, cmap="hot", interpolation="nearest", vmin=0, vmax=1)
+    # plt.colorbar(heatmap)
 
 
-def update(frame):
-    global grid
-    grid = iterate_through_grid(grid, dt, D, dx)
-    heatmap.set_array(grid)
-    return [heatmap]
+    # def update(frame):
+    #     global grid
+    #     grid = iterate_through_grid(grid, dt, D, dx)
+    #     heatmap.set_array(grid)
+    #     return [heatmap]
 
 
-ani = animation.FuncAnimation(fig, update, frames=max_iters, interval=interval, blit=False)
+    # ani = animation.FuncAnimation(fig, update, frames=max_iters, interval=interval, blit=False)
 
 
-plt.title("2D Diffusion Simulation")
-plt.show()
+    # plt.title("2D Diffusion Simulation")
+    # plt.show()
 
 
 T = 5
-L = 10
-D = 0.1
+L = 1
+D = 1
 N = 50
 max_range = 100
 
 dx = L / N
 dt = dx**2 / (4*D)
 
-t_values = np.linspace(0, T, 5)
-x_values = np.linspace(0, L, N)
+t_values = [0.001, 0.01, 0.1, 1]
+x_values = np.linspace(0, L, 100)
+D = 1
 
-values = []
-for x in x_values:
-    values.append(analytic_sol(x, D=1, t=0.1, max_range=100))
+plt.figure(figsize=(10, 8))
+
+for t in t_values:
+    analytic_sum = analytic_sol(x_values, t, D, max_range=10)
+    plt.plot(x_values, analytic_sum, label=f't = {t}')
+
+plt.show()
 
 
