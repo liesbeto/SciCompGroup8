082aefb59d59794d6dfa9b1906f3cb0932b2e78d
fredmloth <frederiekeloth@gmail.com>
2025-02-20 13:54:04 +0100

    Checked with assigment neccesities

--- src/run_vibrating_string.py
+++ src/run_vibrating_string.py
@@ -1,16 +1,18 @@
+"""This Module runs the vibrating string by passing an initial condition function."""
+
 import numpy as np
 
 from vibrating_string import VibratingString
 from visualisation import generate_animation
 
 
-def initial_disp(x, L, N):
+def initial_condition1(x, L, N):
     return np.sin(2*np.pi*x)
 
-def initial_disp2(x, L, N):
+def initial_condition2(x, L, N):
     return np.sin(5*np.pi*x)
 
-def initial_disp3(x, L, N):
+def initial_condition3(x, L, N):
     if (0.2*L < x*N < 0.4*L):
         return np.sin(5*np.pi*x)
     else:
@@ -18,10 +20,14 @@ def initial_disp3(x, L, N):
 
 
 def run_sim(L, T, N, c):
-    displacement_func = initial_disp
+    # Create a vibrating string with passed initial condition
+    displacement_func = initial_condition1
     vib_string = VibratingString(displacement_func, L, T, N, c)
-    vib_string.u = vib_string.compute_next_u(vib_string.u)
+    vib_string.u = vib_string.stepping_method(vib_string.u)
+
+    # Generate an animation
     generate_animation(vib_string)
+
 
 if __name__ == "__main__":
     L = 1.0          
--- src/vibrating_string.py
+++ src/vibrating_string.py
@@ -1,8 +1,12 @@
+"""This module contains a class for Vibrating String according to the ... method."""
+
 import numpy as np
 
+# TO DO: Implement Eulers method to compare with stepping
 
 class VibratingString():
     """
+    Boundaries are assumed fixed 
     L: Length of string
     T: Total time of the simulation
     N: Number of timesteps/ Number of divisions of spatial grid
@@ -11,34 +15,36 @@ class VibratingString():
         self.N = N
         self.c = c
         self.L = L
-        self.spatial, self.dx = self.discretization(L)
-        self.temporal, self.dt = self.discretization(T)
+        self.spatial, self.dx = self.boundary(L)
+        self.temporal, self.dt = self.boundary(T)
         self.time_steps = int(T / self.dt)
         self.set_initial_disp(displacement_func)
        
         if not self.check_stability(c):
             raise ValueError("cfl number greater than 1")
 
-    def discretization(self, endpoint):
-        """discretizing time steps (temporal grid) or delta X (spatial grid)"""
+
+    def boundary(self, endpoint):
+        """discretizing time steps (temporal) or delta X (spatial)"""
         return np.linspace(0, endpoint, self.N+1), endpoint/self.N
+
 
     def check_stability(self, c):
         """Returns True if the timestep length is stable according to Courant-Friedrichs-Law."""
         return (c * self.dt / self.dx) <= 1
+
 
     def set_initial_disp(self, displacement_func):
         u = np.zeros((self.N+1, self.time_steps+1))
         for i in range(len(u)):
             u[i] = displacement_func(self.spatial[i], len(u), self.N)
         
-        u[:, 1] = np.copy(u[:, 0])
-
+        u[:, 1] = np.copy(u[:, 0]) 
         self.u = u
 
-    # Only do once and use loop somewhere else because this is not next this is all?
-    # Remove u and use self.u?
-    def compute_next_u(self, u):
+
+    def stepping_method(self, u):
+        """Computes u according to stepping method by using two most recent time points to calculate next"""
         c_sq = self.c**2
         for n in range(1, self.time_steps):
             for i in range(1, self.N):
